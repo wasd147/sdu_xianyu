0a67c1980e1c00fa1d509d6d60dd28cb5d8154f5
unknown <unknown@example.com>
2021-01-29 18:49:46 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,66 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // img: "https://7364-sdujingtao-5g5x4xcdad107ba4-1304845016.tcb.qcloud.la/images/5-409.jpg?sign=869f96b6186094fd02a6b3f3fcdea7d3&t=1611903342",
+    img: "cloud://sdujingtao-5g5x4xcdad107ba4.7364-sdujingtao-5g5x4xcdad107ba4-1304845016/images/5-409.jpg"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
